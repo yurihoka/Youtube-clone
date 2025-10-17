8c5c0a28a26c94ce38bf3ed1ed3ef35cfc190244
yurikahokama <hokama@engineerforce.io>
2025-10-17 23:22:44 +0900

    create feed component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Video from "./Pages/Video/Video";
 
 const App = () => {
   const [sidebar, setSidebar] = useState(true);
@@ -11,7 +12,7 @@ const App = () => {
       <Navbar setSidebar={setSidebar} />
       <Routes>
         <Route path="/" element={<Home sidebar={sidebar} />} />
-        {/* <Route path="/video/:categoryId/:videoId" element={<Video />} /> */}
+        <Route path="/video/:categoryId/:videoId" element={<Video />} />
       </Routes>
     </div>
   );
--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Video.css";
+import PlayVideo from "../../Components/PlayVideo/PlayVideo";
 
 const Video = () => {
-  return <div></div>;
+  return <div className="play-container">
+    <PlayVideo />
+  </div>;
 };
 
 export default Video;
